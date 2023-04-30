c596701e3c6e5f6961a1415bf42fbfed8a4f536f
rodweleo <leorodwel86@gmail.com>
2023-04-30 16:06:03 +0300

    Fifth commit

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,5 +1,15 @@
 export function Footer() {
   const year = new Date().getFullYear();
 
-  return <footer><hr/>{`Copyright Rodwell Leo ${year}`}</footer>;
+  return (
+    <footer>
+      <hr />
+      <form>
+        <h4>Subscribe to my Newsletter</h4>
+        <input type="email" name="subscriber" placeholder="123@gmail.com" />
+        <button style={{ margin: "5px" }}>Subscribe</button>
+      </form>
+      <p>{`Copyright Rodwell Leo ${year}`}</p>
+    </footer>
+  );
 }
