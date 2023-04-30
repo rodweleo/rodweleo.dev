c6e11eefedb6da6a88ab54c40e0fe8acfc3b800a
rodweleo <leorodwel86@gmail.com>
2023-04-30 10:03:18 +0300

    Adding toggling to the sidebar

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,17 +1,30 @@
-import { NavBar } from "./NavBar";
 import { FaBars } from "react-icons/fa";
+import { useState } from "react";
+import { Link } from "../node_modules/react-router-dom/dist/index";
 
 export function Header() {
-  //const [tab, setActiveTab] = useState(nav_bar.keys);
+  const [isOpen, setIsOpen] = useState(false);
 
   function toggleSidebar() {
-    const nav_bar = <NavBar />;
-    console.log(nav_bar);
+    isOpen === false ? setIsOpen(true) : setIsOpen(false);
   }
   return (
     <header className="header">
       <h3>rodweleo.dev</h3>
-      <NavBar />
+      <div className={`nav_bar ${isOpen == true ? "active" : ""}`}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </div>
       <button onClick={toggleSidebar} style={{ background: "#213547" }}>
         <FaBars className="bars" />
       </button>
