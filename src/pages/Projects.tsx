369b24b13a248595b78d1441627badd69a266179
DenisKibe <kibedenis@gmail.com>
2023-08-21 16:51:43 +0300

    Project links update

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,6 +2,7 @@ import { BsPlayFill } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 
 export default function Projects() {
+
   return (
     <section id="projects">
         <h2>Projects</h2>
@@ -11,8 +12,11 @@ export default function Projects() {
               <h3>Smart Muscle Fitness Center</h3>
               <p>This a smart gym that serves as a digital hub for gym-related activities, offering users a user-friendly and engaging experience while effectively promoting the gym's offerings.</p>
               <p>Technologies used: ReactJS</p>
-              <a href="github-link" className="btn-secondary"><FaGithub/></a>
-              <a href="live-demo-link" className="btn-primary"><BsPlayFill/></a>
+              <div className="project-links">
+                  <a href="github-link" className="btn-secondary"><FaGithub/></a>
+                <a href="https://smartmuscle.vercel.app" className="btn-primary" target="_blank"><BsPlayFill/></a>
+              </div>
+              
             </div>
         </div>
         <div className="project">
@@ -21,9 +25,10 @@ export default function Projects() {
               <h3>StreetMatt</h3>
               <p>This is an ecommerce website that allows it's customers to shop items at a cheap, pocket-friendly and discounted prize.</p>
               <p>Technologies used: ReactJS, NodeJS, DarajaAPI</p>
-              
-              <a href="github-link" className="btn-secondary"><FaGithub/></a>
-              <a href="live-demo-link" className="btn-primary"><BsPlayFill/></a>
+              <div className="project-links">
+                <a href="github-link" className="btn-secondary"><FaGithub/></a>
+                <a href="https://streetmatt.vercel.app" className="btn-primary" target="_blank"><BsPlayFill/></a>
+              </div>
             </div>
             
         </div>
